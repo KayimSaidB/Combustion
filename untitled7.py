13dfc57df13112b1dfa79285c1c2a773d199e25e
Kayim SAID BACAR <kayim.said_bacar@corp.sophia.mines-paristech.fr>
2020-10-21 17:26:13 +0200

    contrainte stock useless

--- untitled7.py
+++ untitled7.py
@@ -59,10 +59,11 @@ E_Prod_Therm = P_Nominal * fc / Eff # Production thermique annuelle
 taux_C_min = 0.1
 prix_under=215*0.0036
 prix_over=430*0.0036
-
+stock_min=365*1500
+#stock_max=stock_min*2
 disponibilite_rv=np.array([1000,7000,22000,29000,52000,57000,143000,21000,313000])
 prix_unitaire_rv=np.array([4,7,10,16,25,31,46,61,76])
-
+cout_fixe_stock=500000 #euros
 prix_total_rv=disponibilite_rv*prix_unitaire_rv
 route_rv=np.array([10,20,30,50,80,100,150,200,250])
 cout_vert=20*[0]
@@ -79,11 +80,17 @@ for i in range(20):
 
 M.update()
 
-activation=M.addVars(20, vtype=gb.GRB.BINARY)
-cout_variable_biomasse=M.addVars(20, vtype=gb.GRB.CONTINUOUS)
+activation=M.addVars(20, vtype=gb.GRB.BINARY) # vaut 
+activation_stockage=M.addVars(20, vtype=gb.GRB.BINARY) # vaut 1 l'annee ou on a debloque du stockage 0 sinon
+activation_stockage_sum=M.addVars(20, vtype=gb.GRB.BINARY) # vaut 1 si on a debloque du stoackage
+
+cout_variable_biomasse=M.addVars(20, vtype=gb.GRB.CONTINUOUS) # valeur du cout variable associé à la biomasse
 ####### Definition des contrainte/an
 ctrs = {}
 profit = {}
+
+M.addConstr(gb.quicksum(activation_stockage)<=1) #contrainte de stock sur l'ensemble de la simulation
+
 for i in range (20):
     
     ctrs['mincharbon', i] = M.addConstr((1-taux_C_min) * mass['charbon', i] - taux_C_min * (gb.quicksum(mass[c,i] for c in combustible if c!='charbon')) >= 0, name='10pourcent_min_charbon')
@@ -99,15 +106,16 @@ for i in range (20):
     #    ctrs['apro_local', i] = M.addConstr(0.4*(mass['vert', i]+mass['recycle', i])-0.6*mass['torrefie', i]>=0 , name='appro_local_min')
     #else:
         #ctrs['apro_local', i] = M.addConstr(mass['torrefie', i] == dispo_apres2025['torrefie'] , name='appro_local_min') 
-   # ctrs['Ctrs prix',i]=M.addConstr(cout_variable_biomasse[i]>=prix_over*gb.quicksum(mass[c,i]*PCI[c] for c in combustible)-10e15*(1-activation[i]))
-   # ctrs['Ctrs prix bis',i]=M.addConstr(0.5*gb.quicksum(mass[c,i] for c in combustible)>=gb.quicksum(mass[c,i] for c in combustible if c!='charbon')-10e15*activation[i])
-   # ctrs["Ctrs prix ter",i]=M.addConstr(cout_variable_biomasse[i]>=prix_under*gb.quicksum(mass[c,i]*PCI[c] for c in combustible))
+    ctrs['Ctrs prix',i]=M.addConstr(cout_variable_biomasse[i]>=prix_over*gb.quicksum(mass[c,i]*PCI[c] for c in combustible)-10e15*(1-activation[i]))
+    ctrs['Ctrs prix bis',i]=M.addConstr(0.5*gb.quicksum(mass[c,i] for c in combustible)>=gb.quicksum(mass[c,i] for c in combustible if c!='charbon')-10e15*activation[i])
+    ctrs["Ctrs prix ter",i]=M.addConstr(cout_variable_biomasse[i]>=prix_under*gb.quicksum(mass[c,i]*PCI[c] for c in combustible))
     ctrs["Ctrs vert 1",i]=M.addConstr(gb.quicksum(disponibilite_rv[k]*lambda_rv[k,i] for k in range(9))>=mass['vert',i])
     ctrs["Ctrs vert 2",i]=M.addConstr(gb.quicksum(lambda_rv[k,i] for k in range(9))<=0.96 )
-
-    
+    ctrs["Contrainte stock 2",i]=M.addConstr(gb.quicksum(mass[c,i] for c in combustible if c!='charbon')<=stock_min+stock_min*gb.quicksum(activation_stockage[k] for k in range(i+1))+stock_min*activation_stockage_sum[i])
+    ctrs["Ctrs stock 3"]=M.addConstr(gb.quicksum(activation_stockage[k] for k in range(i))==activation_stockage_sum[i])
+    # ctrs["Contrainte stock 3",i]=M.addConstr(gb.quicksum(mass[c,i] for c in combustible if c!='charbon')<=10e7+(stock_min-10e7)*(1-activation_stockage[i]))
     cout_vert[i]=gb.quicksum(prix_total_rv[k]*lambda_rv[k,i] for k in range(9))
-    profit[i] = gb.quicksum(unit_profit(c,i) * mass[c,i] for c in combustible)-cout_vert[i] #quota[i]-cout_variable_biomasse[i]
+    profit[i] = gb.quicksum(unit_profit(c,i) * mass[c,i] for c in combustible)-cout_vert[i]+quota[i]-cout_variable_biomasse[i]-cout_fixe_stock*activation_stockage[i]
 
 M.setObjective(gb.quicksum(profit[i]  / (1 + T_act) ** i for i in range(20)))
 M.modelSense = gb.GRB.MAXIMIZE
